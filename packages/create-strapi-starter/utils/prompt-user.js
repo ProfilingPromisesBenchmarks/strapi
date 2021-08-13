9f31a64a160e285888ec31df252bf343e58b882b
Mark Kaylor <mark.kaylor@strapi.io>
2021-08-13 11:51:05 +0200

    filter out templates and starters not flagged for cli

--- packages/create-strapi-starter/utils/prompt-user.js
+++ packages/create-strapi-starter/utils/prompt-user.js
@@ -64,14 +64,15 @@ async function getStarterQuestion() {
     };
   }
 
-  const choices = content.map(option => {
-    const name = option.title.replace('Starter', '');
-
-    return {
-      name,
-      value: `https://github.com/${option.repo}`,
-    };
-  });
+  const choices = content
+    .filter(starter => starter.add_to_cli)
+    .map(starter => {
+      const name = starter.title.replace('Starter', '');
+      return {
+        name,
+        value: `https://github.com/${starter.repo}`,
+      };
+    });
 
   return {
     type: 'list',
