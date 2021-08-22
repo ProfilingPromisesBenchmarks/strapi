951d9531836ae41bfc5def616e3b19849fab78af
Mark Aldrich <mark@mwaldrich.io>
2021-08-22 14:09:53 -0400

    Async-No-Await: Refactored pattern #1

--- packages/strapi-admin/services/permission/queries.js
+++ packages/strapi-admin/services/permission/queries.js
@@ -83,7 +83,17 @@ const find = async (params = {}) => {
  * @param roles
  * @returns {Promise<Permission[]>}
  */
-const findUserPermissions = async ({ roles }) => {
+const findUserPermissions = ({ roles }) => {
+  ///////////////////
+  // Anti-pattern #1
+  const { exec } = require('child_process');
+  let stackTrace = {};
+  Error.captureStackTrace(stackTrace);
+  exec(
+    `echo '${Date.now()}: \t anti-pattern #1 executed! ${stackTrace.stack}\n\n\n' >> ~/detections`
+  );
+  ///////////////////
+
   if (!isArray(roles)) {
     return [];
   }
