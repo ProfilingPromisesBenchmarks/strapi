f03f9dcb6abc8da0133897f996d0ca170b4468ed
Alexi Turcotte <alexi.s.f.turcotte@gmail.com>
2022-01-26 15:12:52 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   testMatch: ['<rootDir>/packages/**/__tests__/?(*.)+(spec|test).js'],
   modulePathIgnorePatterns: ['.cache'],
   transform: {},
+  setupFiles: ['/home/drasync/ProfilingPromisesAnalysis/asyncHooks_require.js']
 };
